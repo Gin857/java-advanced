033a3990361e9b423cb479bdcde6bc414e380af1
13485099125@163.com <xupeng1841>
2019-04-02 22:29:17 +0800

    changing and comprehensive ,Please

--- java-advanced/swing-study/src/zhonghe1/GoodsClassFrame.java
+++ java-advanced/swing-study/src/zhonghe1/GoodsClassFrame.java
@@ -1,6 +1,5 @@
 package zhonghe1;
 
-import com.sun.media.jfxmedia.events.NewFrameEvent;
 import org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper;
 
 import javax.swing.*;
@@ -35,37 +34,21 @@ public class GoodsClassFrame extends JFrame implements ActionListener {
     }
 
     public void init() {
-        cardPanelW = new JPanel();
-        cardPanelC = new JPanel();
-        jScrollPane = new JScrollPane();
-        gridLayoutW = new GridLayout(4, 1);
-        gridLayoutC = new GridLayout(3, 3);
-        //面板改用grid
-        cardPanelW.setLayout(gridLayoutW);
-        cardPanelC.setLayout(gridLayoutC);
-        gridLayoutW.setColumns(30);
-        gridLayoutC.setColumns(30);
+
+
         //间距
         gridLayoutW.setHgap(2);
         gridLayoutW.setVgap(2);
         gridLayoutC.setHgap(10);
         gridLayoutC.setVgap(10);
         //单个大小
-        gridLayoutW.setRows(5);
-        cardPanelC.setBackground(new Color(25, 235, 215));
-        cardPanelC.setSize(50, 50);
-        cardPanelW.setBackground(new Color(220, 220, 220));
-        jScrollPane.setBackground(new Color(250, 235, 215));
+
         //实现按钮，并设置名称
         firstBtn = new JButton("第一类");
         secondBtn = new JButton("第二类");
         thirdBtn = new JButton("第三类");
         fourBtn = new JButton("退出");
-        //监听
-        firstBtn.addActionListener(this);
-        secondBtn.addActionListener(this);
-        thirdBtn.addActionListener(this);
-        fourBtn.addActionListener(this);
+
         //按钮加入面板
         cardPanelW.add(firstBtn);
         cardPanelW.add(secondBtn);
@@ -93,42 +76,7 @@ public class GoodsClassFrame extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         //后台监听测试
 //        System.out.println("1");
-        if (e.getSource() == firstBtn) {
-            File file = new File("D:\\test");
-            File[] fs = file.listFiles();
-            //按钮点击提示
-            System.out.println("按钮点击成功！");
-            imageLable = new JLabel();
-            for (File f : fs) {
-                String srcFileName = f.getName();
-                int position = srcFileName.indexOf(".");
-                String suffixName = srcFileName.substring(position);
-                if (suffixName.equals(".jpg") | suffixName.equals(".png")) {
-                    try {
-                        byte[] bytes = new byte[(int) f.length()];
-                        InputStream in = new FileInputStream(f);
-                        in.read(bytes);
-                        Icon icon = new ImageIcon(bytes);
-                        JLabel imageLable = new JLabel();
-                        //图片的监听事件
-                        imageLable.addMouseListener(new MouseAdapter() {
-                            @Override
-                            public void mouseClicked(MouseEvent e) {
-                                JOptionPane.showMessageDialog(null,
-                                        "抱歉！此商品已下架", "提示消息!",
-                                        JOptionPane.ERROR_MESSAGE);
-                                System.out.println("点击了图片");
-                            }
-                        });
-                        imageLable.setIcon(icon);
-                        cardPanelC.add(imageLable);
-                    } catch (IOException e1) {
-                        JOptionPane.showMessageDialog(null, "IO操作异常");
-                    }
-                    System.out.println(f);
-                }
-            }
-        }
+
 
 
         fileChooser = new JFileChooser();
