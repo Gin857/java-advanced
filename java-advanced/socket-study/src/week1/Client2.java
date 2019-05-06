f4cbae13669812ad6800edd13fc2e8b4a0850748
xupeng4615 <13485099125@163.com>
2019-05-07 06:08:53 +0800

    java网络编程（2）

--- java-advanced/socket-study/src/week1/Client2.java
+++ java-advanced/socket-study/src/week1/Client2.java
@@ -1,26 +1,19 @@
 package week1;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client2 {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("10.30.196.105",10086);
+        Socket socket = new Socket("192.168.43.140", 10086);
         OutputStream out = socket.getOutputStream();
         Scanner scanner = new Scanner(System.in);
-        while (true){
-            System.out.println("请输入你将要向服务器发送的消息：");
+        while (true) {
+            System.out.println("请输入你将要向服务器端发送的消息：");
             String info = scanner.nextLine();
             out.write(info.getBytes());
-            Socket socket1 = new Socket("10.30.196.105",10086);
-            //循环的内容
-            InputStream in = socket1.getInputStream();
-            byte[] bytes = new byte[1024];
-            in.read(bytes);
-            System.out.println("服务器发送了："+new String(bytes));
         }
     }
 }
--- java-advanced/socket-study/src/week1/Server2.java
+++ java-advanced/socket-study/src/week1/Server2.java
@@ -2,34 +2,23 @@ package week1;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Server2 {
     public static void main(String[] args) throws IOException {
         ServerSocket ss = new ServerSocket(10086);
-        System.out.println("服务器端口启动！端口：10086");
+        System.out.println("服务器端口启动！端口为：10086");
         InputStream in;
         while (true) {
             Socket socket = ss.accept();
             byte[] bytes = new byte[1024];
-            System.out.println("客服端" + socket.getInetAddress() + "上线了");
-            in = socket.getInputStream();
-            in.read(bytes);
-            System.out.println("客服端" + socket.getInetAddress() + "给您发来新消息：" + new String(bytes));
-            System.out.println("请输入向客服端将要发送的消息：");
-            Scanner scanner = new Scanner(System.in);
-            String txt = scanner.nextLine();
-            while (true){
-                Socket socket1 = ss.accept();
-                OutputStream out ;
-                System.out.println("客服端："+socket1.getInetAddress()+"上线了");
-                out= socket1.getOutputStream();
-                out.write(txt.getBytes());
+            System.out.println("客户端" + socket.getInetAddress() + "上线了");
+            while (true) {
+                in = socket.getInputStream();
+                in.read(bytes);
+                System.out.println("客户端" + socket.getInetAddress() + "给您发来新消息：" + new String(bytes));
             }
         }
-
     }
 }
