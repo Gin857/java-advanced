b827518a8553b4bce20ead5907e9452a3b4df8fd
xupeng4615 <13485099125@163.com>
2019-05-06 21:59:52 +0800

    java网络编程（1）

--- java-advanced/wed/src/weekdaka/Client.java
+++ java-advanced/wed/src/weekdaka/Client.java
@@ -4,6 +4,7 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws Exception{
+        //host 主机 localhost 本地主机    remotehost远程主机
         Socket socket =new Socket("192.168.43.140",10086);
         System.out.println("以和服务器建立连接，远程地址主机："+socket.getRemoteSocketAddress());
     }
--- java-advanced/wed/src/weekdaka/Server.java
+++ java-advanced/wed/src/weekdaka/Server.java
@@ -1,14 +1,19 @@
 package weekdaka;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException{
+        //创建服务器对象
         ServerSocket ss =new ServerSocket(10086);
         System.out.println("服务器启动，端口号："+ss.getLocalPort());
+        //服务器咋端口监听
         while (true){
+            //获得和客服端对象的连接
             Socket socket = ss.accept();
+            //获得客服端的官网的地址
             System.out.println("客服端："+socket.getInetAddress()+"上线了");
             socket.close();
         }
