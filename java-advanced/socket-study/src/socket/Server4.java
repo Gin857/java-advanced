9ab509227338183901fada6c74484fd92b3185f3
xupeng4615 <13485099125@163.com>
2019-05-09 09:08:11 +0800

    java网络编程（4）

--- java-advanced/socket-study/src/socket/Server4.java
+++ java-advanced/socket-study/src/socket/Server4.java
@@ -11,33 +11,41 @@ public class Server4 {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(10086);
         System.out.println("服务器端口已启动，端口号10086");
-        while (true){
+        while (true) {
             Socket socket = serverSocket.accept();
             ServerThread4 serverThread4 = new ServerThread4(socket);
             new Thread(serverThread4).start();
         }
     }
 }
-class ServerThread4 implements Runnable{
+
+class ServerThread4 implements Runnable {
 
     private Socket socket;
-    public ServerThread4(Socket socket){
+
+    public ServerThread4(Socket socket) {
         this.socket = socket;
     }
+
     @Override
     public void run() {
-        System.out.println("客户端"+socket.getInetAddress()+"连接成功！");
+        System.out.println("客户端" + socket.getInetAddress() + "连接成功！");
         try {
-            File file =new File("D:/space/2.jpg");
+            //首先找文件路径，并且白文件传给流，
+            File file = new File("D:/space/2.jpg");
             InputStream inputStream = new FileInputStream(file);
-            byte[] bytes = new byte[1024];
+            //文件内容写到数组，并且把获取到数据的数组给写给写好的流，流会把数据写道socket中
+            byte[] bytes = new byte[(int) file.length()];
             inputStream.read(bytes);
+            //获取socket中的数据然后输出
             OutputStream outputStream = socket.getOutputStream();
+            outputStream.write(bytes);
+            //关闭各种流
             inputStream.close();
             outputStream.close();
             socket.close();
-        }catch (Exception e){
-            System.out.println("IO异常或打印异常");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
