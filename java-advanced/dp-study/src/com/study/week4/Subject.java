0f0ceb9274a4a2cf0a0c83ac53894f46b5be4639
xupeng4615 <13485099125@163.com>
2019-05-23 06:12:03 +0800

    代理模式

---
+++ java-advanced/dp-study/src/com/study/week4/Subject.java
@@ -0,0 +1,5 @@
+package com.study.week4;
+
+public interface Subject {
+    public void buyMac();
+}
---
+++ java-advanced/dp-study/src/com/study/week4/proxyPattern.java
@@ -0,0 +1,9 @@
+package com.study.week4;
+
+public class proxyPattern {
+    public static void main(String[] args) {
+        //
+        Subject proxy = new Proxy();
+        proxy.buyMac();
+    }
+}
